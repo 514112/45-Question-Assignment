ca90d52464e4946aacd93f82d527ede47eaedbfe
Namal <khannamal440@gmail.com>
2024-07-17 14:02:38 -0700

    45-Assignment-Question

--- Q45/main.js
+++ Q45/main.js
@@ -30,17 +30,14 @@ return car;
 }
 let my_car = car_create ("Toyota","Corolla","Color:White","Sunroof:true")
 */
-function car_brand(manufacturer, model, options) {
-    let car = {
-        manufacturer: manufacturer,
-        model: model,
+Object.defineProperty(exports, "__esModule", { value: true });
+function car_create(manufacturer, model, optional) {
+    return {
+        manufacturer,
+        model,
+        ...optional
     };
-    options.forEach(option => {
-        let [key, value] = option.split(":");
-        car[key.trim()] = value.trim();
-    });
-    return car;
 }
 ;
-let my_car = car_brand("Toyota", "Corolla", "Color:White", "Sunroof:true");
+const my_car = car_create("Toyota", "Corolla", { Color: "White", Sunroof: "true", year: "2024" });
 console.log(my_car);
--- Q45/main.ts
+++ Q45/main.ts
@@ -1,51 +1,16 @@
 
-
-/*function car_brand(manufacturer, model, ...options){
-    let car = {
-        manufacturer : manufacturer,
-        model : model
-    }
-    options.forEach(option => {
-    let [Key,value] = option.split(":")
-    car [Key.trim()] = value.trim()
-});
-
-return car;
-
-let my_car = car_brand ("Toyota","Corolla","Color:White","Sunroof:true")
-console.log(my_car);
-
-}
-
-/*
-function car_create(manufacturer, model, ...options){
-    let car = {
-        manufacturer : manufacturer,
-        model : model
-    }
-    options.forEach(option =>{
-        let [key,value] = option.split(":")
-        car [key.trim()] =value.trim()
-});
-return car;
-} 
-let my_car = car_create ("Toyota","Corolla","Color:White","Sunroof:true")
-*/
-
-
-function car_brand(manufacturer, model, options?){
-    let car = {
-        manufacturer: manufacturer,
-        model: model,
-    }
-    
-    options.forEach(option => {
-        let [key, value] = option.split(":")
-        car [key.trim()] = value.trim()
-    })
-
-    return car;
+type car = {
+    manufacturer: string,
+    model: string,
+    [key:string]:any;
 };
 
-let my_car = car_brand("Toyota", "Corolla", "Color:White", "Sunroof:true");
+function car_create(manufacturer: string,model:string, optional:Record<string,any>): car {
+    return{
+        manufacturer,
+        model,
+        ...optional
+}};
+
+const my_car: car = car_create("Toyota","Corolla",{Color: "White",Sunroof: "true",year: "2024"});
 console.log(my_car);
